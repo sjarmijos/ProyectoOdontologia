ee6b6e483d7c7f283c48431f05738e488df1a1e4
sjarmijos <saccpgm@gmail.com>
2024-06-22 23:23:26 -0500

    Agregando estaticos: index, post_odontologos, get_odontologos | Agregando js: get_odontologos, post_odontologos

---
+++ src/main/resources/static/js/get_odontologos.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", function () {
+  (function  () {
+    const url = "/odontologo";
+    const settings = {
+      method: "GET",
+    };
+
+    fetch(url, settings)
+      .then(response => response.json())
+      .then(data => {
+        for (let odontologo of data) {
+          var table = document.getElementById("odontologosTableBody"); // Asegúrate de que este ID exista en tu HTML
+          var odontologoRow = table.insertRow();
+          let tr_id = odontologo.id;
+          odontologoRow.id = tr_id;
+
+          const deleteButton = '<button' +
+                                ' id=' + '\"' + odontologo.id + '\"' +
+                                ' type="button" onclick="deleteBy('+odontologo.id+')" class="btn btn-circle btn-danger">' +
+                                '<i class="fas fa-trash"></i>' +
+                                '</button>';
+
+          const updateButton = '<button' +
+                                ' id=' + '\"' + 'btn_id_' + odontologo.id + '\"' +
+                                ' type="button" onclick="findBy('+odontologo.id+')" class="btn btn-circle btn-success">' +
+                                '<i class="fas fa-pencil-alt"></i>' +
+                                '</button>';
+          odontologoRow.innerHTML =
+            "<td>" + odontologo.id + "</td>" +
+            '<td class="matricula">' + odontologo.matricula.toUpperCase() + "</td>" +
+            '<td class="nombre">' + odontologo.nombre.toUpperCase() + "</td>" +
+            '<td class="apellido">' + odontologo.apellido.toUpperCase() + "</td>" +
+            '<td class="Acciones">' + updateButton + "  " +deleteButton + "</td>";
+        }
+      })
+      .catch((error) => console.error('Error al obtener los odontólogos:', error));
+  })();
+});
